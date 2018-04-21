1701733823a7ea03eb8c7524506ae50cb0f4b447
Eric (Martin) Mancini <coelomate@gmail.com>
2018-04-21 10:36:33 -0400

    first simple node.js server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+var http = require("http");
+
+const PORT = 4000;
+
+// Callback handling the request from the server and logging the URL hit
+function handleRequest(request, response) {
+  response.end(`Server works! Path: ${request.url}`);
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function() {
+  console.log("Server up and listening: http://localhost:" + PORT);
+});
+
