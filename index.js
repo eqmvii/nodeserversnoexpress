20f31c0b9fc7e41b4fa6c159140e2f9fb1f1ac6b
Eric (Martin) Mancini <coelomate@gmail.com>
2018-04-21 11:05:38 -0400

    raw CSS styles served by the server coooooool

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ const PORT = 4000;
 // Callback handling the request from the server and logging the URL hit
 function handleRequest(request, response) {
     var path = request.url;
+    console.log(path);
 
     switch (path) {
         case "/":
             displayHome(path, request, response);
             break;
+        case "/goodbye":
+            displayGoodbye(path, request, response);
+            break;
+        case "/style.css":
+            sendStyles(path, request, response);
+            break;
         default:
-            response.end(`Server works! Path: ${request.url}`);
+            response.end(`Default... ${request.url}`);
     }
 }
 
@@ -19,16 +26,50 @@ function displayHome(path, req, res) {
     var homeHTML = `
     <html>
         <head>
+            <link rel="stylesheet" type="text/css" href="style.css">
             <title>Node.js Server Testing</title>
         </head>
         <body>
             <h1>Hello World!</h1>
             <p>This is the root.</p>
+            <a href="/goodbye">Goodbye</a>
         </body>
     </html>
     `;
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(homeHTML);
+}
+
+function displayGoodbye(path, req, res) {
+    var goodbyeHTML = `
+    <html>
+        <head>
+            <link rel="stylesheet" type="text/css" href="style.css">
+            <title>Goodbye!</title>
+        </head>
+        <body>
+            <h1>Goodbye World!</h1>
+            <p>See you later!</p>
+            <a href="/">Return Home</a>
+        </body>
+    </html>
+    `;
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(goodbyeHTML);
+}
+
+function sendStyles(path, req, res) {
+    var styles = `
+    body {
+      color: blue;
+    }
+
+    h1 {
+        text-align: center;
+    }
+    `;
+    // res.writeHead(200, "content")
+    res.end(styles);
 }
 
 var server = http.createServer(handleRequest);
