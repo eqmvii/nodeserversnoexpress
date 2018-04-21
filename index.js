3a5b5981d6ebe2f765da15222b7ad7bc79f056f7
Eric (Martin) Mancini <coelomate@gmail.com>
2018-04-21 10:41:08 -0400

    two servers are better than one

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 var http = require("http");
 
-const PORT = 4000;
+const PORT_ONE = 3000;
+const PORT_TWO = 4000;
 
 // Callback handling the request from the server and logging the URL hit
-function handleRequest(request, response) {
-  response.end(`Server works! Path: ${request.url}`);
+function handleRequestPortOne(request, response) {
+  response.end(`Server One works! Path: ${request.url}`);
 }
 
-var server = http.createServer(handleRequest);
+function handleRequestPortTwo(request, response) {
+    response.end(`Server Two works! Path: ${request.url}`);
+  }
 
-server.listen(PORT, function() {
-  console.log("Server up and listening: http://localhost:" + PORT);
+var serverOne = http.createServer(handleRequestPortOne);
+var serverTwo = http.createServer(handleRequestPortTwo);
+
+
+serverOne.listen(PORT_ONE, function() {
+  console.log(`Server #1 is up and listening: http://localhost: ${PORT_ONE}`);
+});
+
+serverTwo.listen(PORT_TWO, function() {
+  console.log(`Server #2 is up and listening: http://localhost: ${PORT_TWO}`);
 });
 
